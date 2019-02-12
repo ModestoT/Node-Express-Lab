66c38b605fc61cab96c552ad24980a04874c0a07
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-12 15:52:56 -0600

    installed React

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const postRouter = require('./posts/posts-router.js');
+const cors = require('cors');
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+
 server.use('/api/posts', postRouter);
 
 module.exports = server;
